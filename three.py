4e8cf1694cc01b3ba3797217c32694f174cde5f1
Ivan Vasilevskiy <fhgoik@gmail.com>
2023-10-07 21:58:23 +0300

    last commit

---
+++ three.py
@@ -0,0 +1,15 @@
+#
+print("Hello world")
+print("Здесь все так как надо 0/-\0")
+################
+#rigoeregkemgege
+a = 10
+S = 5
+print(S)
+print("ia ustal")
+print("Aliluia")
+#gjngww
+print("zdes oshibka ia imel v vidu 'dobila' -> 'dobilsia' [horocho]")
+print("Proizvozhu proverku na branch-venkah [pozhelayte mne udachi]")
+###
+u1dfbhw
